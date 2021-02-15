1cbc8650aa112c57ecdfdebb735d141ce0c97299
Andrew <andrey.chiganaev@yandex.ru>
2021-02-15 19:55:25 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,153 @@
+//"use strict";
+
+// let number = 5;
+// const LeftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     a: 50
+// };
+
+// obj.a = 10;
+
+// console.log(obj);
+
+
+// console.log(name);
+// var name = 'Ivan';
+
+// {
+//     var result = 50;
+// }
+
+// console.log(result);
+
+// alert(5);
+// [].push('a');
+
+
+
+// ------------------------------------------------------------------------------
+
+
+
+// "use strict";
+
+// let number = 4.6;
+// console.log(-4/0);
+// console.log('string' * 9);
+
+// const persone = 'Alex';
+
+// const bool = true;
+
+// // console.log(something);
+
+// let und;
+// console.log(und);
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(obj.name);
+// // console.log(obj ['name']);
+
+
+// let arr = ['plump.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+// console.log(arr[1]);
+
+
+
+// ------------------------------------------------------------------------------
+
+
+
+// "use strict";
+
+// alert('Hello');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18?", "18");
+// console.log(answer);
+// console.log(typeof(answer));
+// console.log(answer + 5);
+// console.log(answer + 5);    // Перед prompt ставим +, тем самым преобразовали answer со строки в число
+
+// const answers = [];
+
+// answers[0] = prompt('Как Ваше имя?', '');
+// answers[1] = prompt('Как Ваша фамилия', '');
+// answers[2] = prompt('Сколько Вам лет?', '');
+
+// document.write(answers);
+// console.log(typeof(answers));
+
+
+
+// ----------------------------------------------------------------------------------------------
+
+
+
+// "use strict";
+
+// const category = 'toys';
+
+// // console.log('https://someurl.com/' + category + '/' + '4');
+// console.log(`https://someurl.com/${category}/4`); // Этот прием работает только при использовании косых ковычек `...`
+
+// const user = 'Ivan';
+
+// alert(`Привет, ${user}`);
+
+
+
+// ---------------------------------------------------------------------------------------------
+
+
+
+// "use strict";
+
+// console.log(4 + +'5'); // Плюсом мы меняем тип строки на число и так на выходе получаем не строку 45, а число 9
+
+// let incr = 10,
+//     decr = 10;
+
+// incr++; // Оператор инкримента, т.е. увеличение значения на 1
+// decr--; // Оператор декримента, т.е. уменьшение значения на 1
+// ++incr;
+// --decr;
+
+// console.log(incr);
+// console.log(decr);
+// console.log(incr++); // Сначала используем, потом прибавляем. Вывод остается 10
+// console.log(decr--); // Сначала используем, потом отнимаем. Вывод остается 10
+// console.log(++incr); // Сначала прибавляем, потом используем. Вывод 11
+// console.log(--decr); // Сначала отнимаем, потом используем. Ввод 9
+
+// console.log(5%2); // Определяем остаток. Пять разделили на 2 столько, на сколько это возможно и получили остаток 1
+
+// console.log(2*4 == 8); // Получим результат true, потому что это верно и значение булиновое
+// console.log(2*4 == '8'); // Тоже будет работать. Будет true
+// console.log(2*4 === '8'); // Используем строгое сравнение (===), сравнивает также тип значения. Результат false
+
+
+// const isChecked = true,
+//       isClose = false;
+
+// console.log(isChecked && isClose);
+// console.log(isChecked || isClose);
+// console.log(isChecked && !isClose); // Поставили оператор отрицания (!). Значение поменялось на противоположное
+
+// console.log(2 + 2 * 2 != 8); // 6 не равно 8. Результат true
+
+
+// ------------------------------------------------------------------------------------------------
+
+
